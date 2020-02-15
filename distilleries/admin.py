9bdb0791a2a46d3fef7689bd5e62dbddf7f7565e
dpfrakes <dpfrakes@gmail.com>
2020-02-15 02:13:09 -0500

    add distillery model

--- distilleries/admin.py
+++ distilleries/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from distilleries.models import Distillery
+
+class DistilleryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Distillery, DistilleryAdmin)
--- distilleries/models.py
+++ distilleries/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Distillery(models.Model):
+    name = models.CharField(
+        max_length=50,
+        blank=True, null=True)
+    location = models.CharField(
+        max_length=40,
+        blank=True, null=True)
+    region = models.CharField(
+        max_length=40,
+        blank=True, null=True)
+    owner = models.CharField(
+        max_length=60,
+        blank=True, null=True)
+    latitude = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        blank=True, null=True)
+    longitude = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        blank=True, null=True)
+
+    class Meta:
+    	verbose_name_plural = 'Distilleries'
