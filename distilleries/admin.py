f324b122216ce8c26e836e5e695e05ce0e6120ce
dpfrakes <dpfrakes@gmail.com>
2020-02-15 02:31:50 -0500

    fix admin

--- distilleries/admin.py
+++ distilleries/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from distilleries.models import Distillery
 
 class DistilleryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'location', 'region')
+    list_filter = ('region',)
+    ordering = ('name',)
 
 admin.site.register(Distillery, DistilleryAdmin)
