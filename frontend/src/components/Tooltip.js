78977b6392f79946abfa0a1dd714c7bbd71d4630
Dan Frakes <dpfrakes@gmail.com>
2020-05-17 02:09:02 -0400

    wip tooltip updates

--- frontend/src/components/Tooltip.js
+++ frontend/src/components/Tooltip.js
@@ -9,27 +9,26 @@ class Tooltip extends Component {
 
     const info = this.props.entity.info;
 
-    return this.props.entity.type == "distillery" ? (
-      <div className="tooltip" style={{backgroundImage: `url(${info.image || defaultBackground})`}}>
-        <div className="entity-info">
-          <h2>{info.name}</h2>
-          <p>{info.owner}</p>
-          <p>{info.region}</p>
-          <p>{info.coordinates}</p>
-          <p>{info.year_established}</p>
-        </div>
+    return (
+      <div className="tooltip">
+        <div className="image" style={{backgroundImage: `url(${info ? info.image || defaultBackground : defaultBackground})`}}></div>
+          {this.props.entity.type == "distillery" ? (
+            <div className="entity-info">
+              <h2>{info.name}</h2>
+              <p>{info.owner}</p>
+              <p>{info.region}</p>
+              <p>{info.coordinates}</p>
+              <p>{info.year_established}</p>
+            </div>
+          ) : (this.props.entity.type == "company" ? (
+            <div className="entity-info">
+              <h2>{info.name}</h2>
+              {info.distilleries.map((d, i) =>
+                <React.Fragment key={i}>{d.name}<br/></React.Fragment>
+              )}
+            </div>
+          ) : <></>)}
       </div>
-    ) : (
-      this.props.entity.type == "company" ? (
-        <div className="tooltip" style={{backgroundImage: `url(${info.image || defaultBackground})`}}>
-          <div className="entity-info">
-            <h2>{info.name}</h2>
-            {info.distilleries.map((d, i) =>
-              <React.Fragment key={i}>{d.name}<br/></React.Fragment>
-            )}
-          </div>
-        </div>
-      ) : <></>
     )
   }
 
